77d46e77b6b3981455b8d9eda3c93cc684b1b11d
Andris Murnieks <amurnieks4@gmail.com>
2021-01-22 03:27:38 -0500

    Like Button

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,43 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Card, Icon, Label, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
 
+import { AuthContext } from '../context/auth';
+import LikeButton from './LikeButton';
 
-function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes }}){
-  function likePost(){
-    console.log('Like Post!!')
-  }
 
-  function commentOnPost(){
-    console.log("Comment on post!!!")
-  }
+function PostCard({
+  post: { body, createdAt, id, username, likeCount, commentCount, likes }
+}) {
+  const { user } = useContext(AuthContext);
 
   return (
-    <Card>
+    <Card fluid>
       <Card.Content>
-        <Image floated="right" size="mini" src="https://react.semantic-ui.com/images/avatar/large/molly.png"/>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+        />
         <Card.Header>{username}</Card.Header>
-            <Card.Meta>{moment(createdAt).fromNow()}
-            </Card.Meta>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Button as ="div" labelPosition="right" onClick={likePost}>
-        <Button color = 'teal' basic>
-          <Icon name='heart'/>
-        </Button>
-        <Label  basic color = 'teal' pointing='left'>
-          {likeCount}
-        </Label>
-        </Button>
-        <Button as ="div" labelPosition="right" onClick={commentOnPost}>
-        <Button color = 'blue' basic>
-          <Icon name='comments'/>
-        </Button>
-        <Label  basic color = 'blue' pointing='left'>
-          {commentCount}
-        </Label>
-        </Button>
+        <LikeButton user = { user } post={{ id, likes, likeCount }} />
+          <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
+            <Button color="blue" basic>
+              <Icon name="comments" />
+            </Button>
+            <Label basic color="blue" pointing="left">
+              {commentCount}
+            </Label>
+          </Button>
+          {user && user.username === username &&(
+            <Button as="div" color = "red" floated = "right" onClick = {()=> console.log('Delete Post')}>
+              <Icon name = "trash" style = {{margin: 0}} />
+            </Button>
+          )}
       </Card.Content>
     </Card>
   );
--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,64 +1,82 @@
 import React from 'react';
 import { Button, Form } from 'semantic-ui-react';
 import gql from 'graphql-tag';
-import { useMutation } from '@apollo/react-hooks'
+import { useMutation } from '@apollo/react-hooks';
 
 import { useForm } from '../util/hooks';
 import { FETCH_POSTS_QUERY } from '../util/graphql';
 
-function PostForm(){
+function PostForm() {
+  const { values, onChange, onSubmit } = useForm(createPostCallback, {
+    body: ''
+  });
 
-    const { values, onChange, onSubmit} = useForm(createPostCallBack,{
-        body: ''
-    });
-
-    const [createPost, { error }] = useMutation(CREATE_POST_MUTATION,{
-        variables: values,
-        update(proxy,result){
-            const data = proxy.readQuery({
-                query: FETCH_POSTS_QUERY
-            })
-            data.getPosts = [result.data.createPost, ...data.getPost];
-            proxy.writeQuery({query: FETCH_POSTS_QUERY, data})
-            values.body = ''
-        }
-    });
-
-    function createPostCallBack(){
-        createPost()
+  const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+    variables: values,
+    update(proxy, result) {
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY
+      });
+      data.getPosts = [result.data.createPost, ...data.getPosts];
+      proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
+      values.body = '';
     }
+  });
 
-    return(
-        <Form onSubmit = {onSubmit}>
-            <h2>Create a post:</h2>
-            <Form.Field>
-                <Form.Input
-                    placeholder="Hi World!"
-                    name = "body"
-                    onChange = {onChange}
-                    value = {values.body}
-                    />
-                <Button type = "submit" color = "teal">
-                    Submit
-                </Button>
-            </Form.Field>
-        </Form>
-    )
+  function createPostCallback() {
+    createPost();
+  }
+
+  return (
+    <>
+      <Form onSubmit={onSubmit}>
+        <h2>Create a post:</h2>
+        <Form.Field>
+          <Form.Input
+            placeholder="Hi World!"
+            name="body"
+            onChange={onChange}
+            value={values.body}
+            error={error ? true : false}
+          />
+          <Button type="submit" color="teal">
+            Submit
+          </Button>
+        </Form.Field>
+      </Form>
+      {error && (
+        <div className="ui error message" style={{ marginBottom: 20 }}>
+          <ul className="list">
+            <li>{error.graphQLErrors[0].message}</li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
 
 const CREATE_POST_MUTATION = gql`
-mutation createPost($body: String!){
-    createPost(body: $body){
-    id body createdAt username
-    likes{
-        id username createdAt
+  mutation createPost($body: String!) {
+    createPost(body: $body) {
+      id
+      body
+      createdAt
+      username
+      likes {
+        id
+        username
+        createdAt
+      }
+      likeCount
+      comments {
+        id
+        body
+        username
+        createdAt
+      }
+      commentCount
     }
-    likeCount
-    comments{
-        id body username createdAt
-    }
-    commentCount
-    }
-}
-`
+  }
+`;
+
 export default PostForm;
